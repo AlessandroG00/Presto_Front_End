25fe6f8c9115a635bc61493e931da8287f1a3342
Matteo Affatato <matteoaffatato@MacBook-Air-di-Matteo.local>
2024-01-04 16:38:35 +0100

    fixata navbar e ultimi annunci

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector('#navbar');
 const navLinks = document.querySelectorAll('.nav-link')
-const navBrand = document.querySelector('.navbar-brand');
+const navBrand = document.querySelector('#logo');
 const navIcon = document.querySelector('#icon-nav');
 const btnPrimary = document.querySelector('#btnPrimary');
 
@@ -11,7 +11,7 @@ window.addEventListener('scroll',() => {
             link.style.color = 'white'
             navIcon.style.color='white'
         })
-        navBrand.style.color = 'white'
+        navBrand.style.color = 'white';
         btnPrimary.style.backgroundColor = 'white'
         btnPrimary.style.color= 'black'
     } else {
@@ -124,9 +124,9 @@ const reviewsWrapper = document.querySelector('#reviewsWrapper');
 
 let reviews = [
     {'name' : 'Matteo', 'review' : 'Mi sono trovato benissimo con presto', 'rank' : 4.5, 'url' : '/media/Size=05.png'},
-    {'name' : 'Federico', 'review' : 'Ottimi prodotti servizio spedizione veloce', 'rank' : 5, 'url' : '/media/Size=05.png'},
-    {'name' :'Alessandro', 'review' :'Serviizo assistenza clienti eccellente', 'rank' : 4, 'url' : '/media/Size=05.png'},
-    {'name' : 'Giovanni', 'review' :'Non mi è piaciuto il servizio spezione', 'rank' : 2, 'url' : '/media/Size=05.png'},
+    {'name' : 'Federico', 'review' : 'Ottimi prodotti, il servizio di spedizione veloce', 'rank' : 5, 'url' : '/media/Size=05.png'},
+    {'name' :'Alessandro', 'review' :'Assistenza clienti eccellente', 'rank' : 4, 'url' : '/media/Size=05.png'},
+    {'name' : 'Giovanni', 'review' :'Non mi è stato accettato il reso', 'rank' : 2, 'url' : '/media/Size=05.png'},
 ]
 
 reviews.forEach((review) => {
